2034d8dd26a7d45e84d5c73756decd339017f8e9
Alex <ronaldalexandervacca@unicesar.edu.co>
2022-11-21 19:40:15 -0500

    autenticacion v1

---
+++ lib/ui/chat/salachat.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class SalaChat extends StatefulWidget {
+  const SalaChat({super.key});
+
+  @override
+  State<SalaChat> createState() => _SalaChatState();
+}
+
+class _SalaChatState extends State<SalaChat> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Chat Firebase'),
+      ),
+    );
+  }
+}
